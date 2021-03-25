6873db774124d1a482ac3b16d8f113c96d1bb6a8
Evgenia Golubeva <evgenia.m.golubeva@gmail.com>
2021-03-25 12:33:29 +0100

    add time command help

--- Examples/TimeExample.cs
+++ Examples/TimeExample.cs
@@ -2,6 +2,30 @@ using System;
 
 namespace CShargs.Examples
 {
+    
+    /*
+     * time [options] command [arguments...]
+
+    GNU Options
+        -f FORMAT, --format=FORMAT
+               Specify output format, possibly overriding the format specified
+               in the environment variable TIME.
+        -p, --portability
+               Use the portable output format.
+        -o FILE, --output=FILE
+               Do not send the results to stderr, but overwrite the specified file.
+        -a, --append
+               (Used together with -o.) Do not overwrite but append.
+        -v, --verbose
+               Give very verbose output about all the program knows about.
+
+    GNU Standard Options
+        --help Print a usage message on standard output and exit successfully.
+        -V, --version
+               Print version information on standard output, then exit successfully.
+        --     Terminate option list.
+     */
+    
 
     [ParserSettings1(ParserConst.LONG_USE_EQUAL | ParserConst.ALLOW_MERGE)]
     class TimeArguments : Parser {
