ca60d4ba44e37a1f0cc3011ebe917445464a6b10
Evgenia Golubeva <evgenia.m.golubeva@gmail.com>
2021-04-11 23:26:11 +0200

    add to verb option example

--- Examples/GitExample.cs
+++ Examples/GitExample.cs
@@ -5,15 +5,18 @@ namespace CShargs.Examples
 
     class GitArguments : Parser {
 
-        [FlagOption("version")] 
+        [FlagOption("version", shortName: 'v', help: "Display version.")] 
         private bool showVersion { get; set; }
 
-        [VerbOption("push")]
+        [VerbOption("push", help: "Push changes to origin.")]
         private GitPushArguments pushArguments { get; set; }
+        
+        // git push --force
     }
 
     class GitPushArguments : Parser {
-
+        [FlagOption("force", shortName:'f', help:"Force push.")]
+        private bool Force { get; set; }
     }
 
 }
