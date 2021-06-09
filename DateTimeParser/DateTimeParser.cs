5ce507d4f1dfe4d38a062b35abc0105b66391ee1
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2021-06-09 10:19:44 +0200

    Import final review submission from s39

---
+++ DateTimeParser/DateTimeParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CShargs;
+
+namespace DateTimeParser
+{
+    /// <summary>
+    /// Parse a DateTime object from the provided date string and print it in the provided format.
+    /// For all the valid input formats see Microsoft docs for DateTime.Parse(string input) and DateTime.ToString(string format)
+    /// </summary>
+    [ParserConfig(commandName:"extension_app")]
+    class DateTimeParser : Parser
+    {
+        [ValueOption("date", required: true, shortName: 'd', help: "A date and time in any reasonable format")]
+        public DateTime Date { get; set; }
+
+        [ValueOption("format", required: true, shortName: 'f', help: "A format string to use when printing the date")]
+        public string Format { get; set; }  
+    }
+}
---
+++ DateTimeParser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CShargs;
+
+
+namespace DateTimeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new DateTimeParser();
+            try
+            {
+                parser.Parse(args);
+            }
+            catch(ParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine(parser.GenerateHelp());
+                Console.WriteLine(parser.GenerateHelp(false));
+                return;
+            }
+
+            Console.WriteLine(parser.Date.ToString(parser.Format));
+        }
+    }
+}
